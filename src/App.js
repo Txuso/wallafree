44a3ca6cc5e4b78527bf659d2f1dda835b6dfe5a
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-10-20 18:59:07 +0200

    add global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-
+import { GlobalStyle } from './global.styles';
 import React from 'react';
 
 function App() {
-  return (
-    <h1>Wallafree</h1>
-  );
+	return (
+		<div>
+			<GlobalStyle />
+
+			<h1>Wallafree</h1>
+			<p>
+				Compiled successfully! You can now view create-react-app in the
+				browser. Local: http://localhost:3000/ On Your Network:
+				http://192.168.1.33:3000/ Note that the development build is not
+				optimized. To create a production build, use yarn build.
+				Compiled successfully! You can now view create-react-app in the
+				browser. Local: http://localhost:3000/ On Your Network:
+				http://192.168.1.33:3000/ Note that the development build is not
+				optimized. To create a production build, use yarn build.
+			</p>
+		</div>
+	);
 }
 
 export default App;
