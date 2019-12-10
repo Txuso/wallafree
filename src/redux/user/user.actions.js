1e0c054309625a4912f940c4fd954986faa2d2bf
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2019-12-10 14:32:35 +0100

    add redux boilerplate files

---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,57 @@
+import UserActionTypes from './user.types';
+
+export const setCurrenUser = (user) => ({
+	type: UserActionTypes.SET_CURRENT_USER,
+	payload: { user: user }
+});
+
+export const googleSignInStart = () => ({
+	type: UserActionTypes.GOOGLE_SING_IN_START
+});
+
+export const signInSuccess = (user) => ({
+	type: UserActionTypes.SING_IN_SUCCESS,
+	payload: { user: user }
+});
+
+export const signInFailure = (error) => ({
+	type: UserActionTypes.SING_IN_FAILURE,
+	payload: { error: error }
+});
+
+export const emailSignInStart = (email, password) => ({
+	type: UserActionTypes.EMAIL_SIGN_IN_START,
+	payload: { email: email, password: password }
+});
+
+export const checkUserSession = () => ({
+	type: UserActionTypes.CHECK_USER_SESSION
+});
+
+export const signOutStart = () => ({
+	type: UserActionTypes.SIGN_OUT_START
+});
+
+export const signOutSuccess = () => ({
+	type: UserActionTypes.SIGN_OUT_SUCCESS
+});
+
+export const signOutFailure = (error) => ({
+	type: UserActionTypes.SIGN_OUT_FAILURE,
+	payload: { error: error }
+});
+
+export const signUpStart = (userCredentials) => ({
+	type: UserActionTypes.SIGN_UP_START,
+	payload: userCredentials
+});
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+	type: UserActionTypes.SIGN_UP_SUCCESS,
+	payload: { user, additionalData }
+});
+
+export const signUpFailure = (error) => ({
+	type: UserActionTypes.SIGN_UP_FAILURE,
+	payload: { error: error }
+});
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,34 @@
+import UserActionTypes from './user.types';
+
+const INITIAL_STATE = {
+	currentUser: null,
+	error: null
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case UserActionTypes.SING_IN_SUCCESS:
+			return {
+				...state,
+				currentUser: action.payload.user,
+				error: null
+			};
+		case UserActionTypes.SIGN_OUT_SUCCESS:
+			return {
+				...state,
+				currentUser: null,
+				error: null
+			};
+		case UserActionTypes.SIGN_OUT_FAILURE:
+		case UserActionTypes.SIGN_UP_FAILURE:
+		case UserActionTypes.SING_IN_FAILURE:
+			return {
+				...state,
+				error: action.payload.error
+			};
+		default:
+			return state;
+	}
+};
+
+export default userReducer;
