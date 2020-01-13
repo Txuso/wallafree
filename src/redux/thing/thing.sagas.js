e1ebc8d4c5d27fffd37cc3e3c131389f68806465
Txuso-dev <txuso-dev@MacBook-Pro-3.local>
2020-01-13 18:42:50 +0100

    allow users to upload empty images

--- src/redux/thing/thing.sagas.js
+++ src/redux/thing/thing.sagas.js
@@ -20,14 +20,9 @@ import { changeModalVisibility } from '../app/app.actions';
 
 export function* fetchThingsAsync() {
 	try {
-		const collectionRef = yield firestore
-			.collection('things')
-			.where('status', '==', 'active')
-			.get();
+		const collectionRef = yield firestore.collection('things').where('status', '==', 'active').get();
 
-		const convertedCollectionArray = convertCollectionToArray(
-			collectionRef
-		);
+		const convertedCollectionArray = convertCollectionToArray(collectionRef);
 
 		yield put(getThingsSuccess(convertedCollectionArray));
 	} catch (error) {
@@ -38,13 +33,17 @@ export function* fetchThingsAsync() {
 export function* addThingAsync(action) {
 	try {
 		const { imageUrl, name } = action.payload.thing;
-		const generatedImageUrl = yield addFilesToStorage({
-			name: name,
-			imageToProcess: imageUrl
-		});
+		let generatedImageUrl =
+			'https://firebasestorage.googleapis.com/v0/b/wallafree-app.appspot.com/o/images%2FUun%20Eric%20salvaje%222020-01-10T11%3A48%3A11.919Z%22?alt=media&token=3c8403ba-f75c-419a-aa7e-45ae1b955b0f';
+		if (imageUrl) {
+			generatedImageUrl = yield addFilesToStorage({
+				name: name,
+				imageToProcess: imageUrl
+			});
+		}
 
 		action.payload.thing.imageUrl = generatedImageUrl;
-		yield call(addCollectionAndDocuments, 'things', [action.payload.thing]);
+		yield call(addCollectionAndDocuments, 'things', [ action.payload.thing ]);
 		yield put(addThingSuccess(action.payload.thing));
 	} catch (error) {
 		yield put(addThingFailure(error.message));
@@ -53,20 +52,13 @@ export function* addThingAsync(action) {
 
 export function* giveThingAsync(action) {
 	try {
-		yield call(addCollectionAndDocuments, 'deals', [
-			{ ...action.payload, timestamp: new Date().getTime() }
-		]);
+		yield call(addCollectionAndDocuments, 'deals', [ { ...action.payload, timestamp: new Date().getTime() } ]);
 
-		yield firestore
-			.collection('things')
-			.doc(action.payload.thingId)
-			.update({
-				status: 'inactive'
-			});
+		yield firestore.collection('things').doc(action.payload.thingId).update({
+			status: 'inactive'
+		});
 
-		const chatsToDelete = yield firestore
-			.collection('chats')
-			.where('thingId', '==', action.payload.thingId);
+		const chatsToDelete = yield firestore.collection('chats').where('thingId', '==', action.payload.thingId);
 
 		deleteDocuments(chatsToDelete);
 		yield put(giveThingSuccess(action.payload.thingId));
@@ -87,9 +79,5 @@ export function* giveThingStart() {
 	yield takeLatest(ThingActionsTypes.GIVE_THING, giveThingAsync);
 }
 export function* thingSagas() {
-	yield all([
-		call(getThingsCollectionsStart),
-		call(addThingStart),
-		call(giveThingStart)
-	]);
+	yield all([ call(getThingsCollectionsStart), call(addThingStart), call(giveThingStart) ]);
 }
